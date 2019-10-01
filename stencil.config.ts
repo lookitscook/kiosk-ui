ba50c4cabaf67e290a9b39902e24f1cf877a11d1
Matthew Cook <matt@cook.company>
2019-09-30 21:35:57 -0500

    v1.0.1 fix issue with fontawesome (sign-in icon)

--- stencil.config.ts
+++ stencil.config.ts
@@ -5,7 +5,10 @@ export const config: Config = {
   outputTargets: [
     {
       type: 'dist',
-      esmLoaderPath: '../loader'
+      esmLoaderPath: '../loader',
+      copy: [
+        { src: 'assets', warn: true }
+      ]
     },
     {
       type: 'docs-readme'
